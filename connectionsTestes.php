96e7dfbddbae237cfb81f5a61243e1c2e73f1234
Renato Alves Soares <renatoguara2020@gmail.com>
2022-12-20 20:30:33 -0300

    ok

--- connectionsTestes.php
+++ connectionsTestes.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
-if (isset($_POST['username']) && ($_POST['password'])) {
+if (isset($_POST['username']) && isset($_POST['password'])) {
 
     $erros = array();
     // Obtém valores do formulário do arquivo login.php
+    $name = strip_tags(htmlspecialchars($_POST['name']));
     $username = filter_input(INPUT_POST,'username', FILTER_SANITIZE_EMAIL);
     $password= filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
     //verifica se alguma variavel é uma variável vazia
@@ -11,11 +12,11 @@ if (isset($_POST['username']) && ($_POST['password'])) {
         //$password = md5($password);
         echo $erros[] = "<li>Todos os campos são obrigatórios! </li>";
     } else {
-        //conexão 
+        //conexão  Successfull and Happy path
         $pdo = new PDO('mysql:host=localhost;dbname=testes_db', 'root', '');
         $query = $pdo->prepare("SELECT * FROM usuarios WHERE username = :username AND password1 = :password1");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        $query->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_ASSOC);
+        $query->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $query->bindValue(':username', $username, PDO::PARAM_STR);
         $query->bindValue(':password1', $password, PDO::PARAM_STR);
 
@@ -23,13 +24,15 @@ if (isset($_POST['username']) && ($_POST['password'])) {
 
         $num = $query->rowCount();
         if ($num == 1) { 
-            echo "Login success!!! Username $username e $password";
+            echo "<li>Login success!!! Username $username e $password </li>";
+            http_response_code(200);
             
 
             echo "Login success!!!$username and  Password $password";
 
         } else {
             echo $erros[] = "Failed to login";
+            http_response_code(500);
         }
     }
 
--- crudPDO.php
+++ crudPDO.php
@@ -1,3 +1,45 @@
+<?php
+if(($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['btn-enviar'])){
+
+if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email'])){
+
+
+    
+
+    $firstname = filter_input(INPUT_POST,'firstname',FILTER_SANITIZE_SPECIAL_CHARS);
+    $lastname = filter_input(INPUT_POST,'lastname',FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
+
+    try{
+
+    //$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $dsn = new PDO('mysql:host=localhost;dbname=testes_db', 'root', '');
+    $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $stmt = $dsn->prepare('INSERT INTO (firstname, lastname, email ) VALUES(:firstname, :lastname, :email)');
+
+    $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+    $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+    
+    }catch(PDOException $e){
+
+  echo  "Error:   $e->getMessage()  $e->$getCode() ";
+  http_response_code(500);
+
+    }
+    
+}
+
+} 
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +58,7 @@
         <input type="text" name="lastname"></input>
         <label>Email</label>
         <input type="text" name="email"></input>
-        <input type="submit">Enviar</input>
+        <input type="submit" name="btn-enviar" value="Enviar" />
     </form>
 </body>
 
